b997d538e19732d113d563bf6a42e5d269380a52
RobertWalt <rob.wa@posteo.de>
2020-01-25 19:44:52 +0100

    Changes in model pipeline and Webapp.

--- App/webapp.py
+++ App/webapp.py
@@ -1,22 +1,41 @@
 # Import packages
-import os
+import sys
 import pickle
-from flask import Flask
+import sys
+
+sys.path.append('/Users/robertwalter/PycharmProjects/Boston')
+from flask import Flask, jsonify, request, render_template, redirect
+from model import BostonPrediction
+from wtforms import Form, SelectField, FloatField, validators
 
 
+class HousingPriceForm(Form):
+    lot_area = FloatField('LotArea', validators=[validators.InputRequired()])
+    bldg_type = SelectField('BldgType', choices=[('1Fam', '1Fam')], validators=[validators.InputRequired()])
 
-class Webapp():
 
-    def create_app(self):
-        # create flask webapp
-        app = Flask(__name__, instance_relative_config=True)
-        app.config.from_mapping(
-            SECRET_KEY='Boston',
-        )
-        # ensure the instance folder exists
-        try:
-            os.makedirs(app.instance_path)
-        except OSError:
-            pass
+def create_app():
+    # create flask webapp
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='Boston',
+    )
 
-        model = pickle.load(open('model.pkl', 'rb'))
+    model = BostonPrediction()
+    model.load_model()
+
+    @app.route('/', methods=['POST', 'GET'])
+    def main():
+        hpform = HousingPriceForm(request.form)
+        if request.method == 'POST' and hpform.validate():
+            lot_area = hpform.lot_area.data
+            building_type = hpform.bldg_type.data
+            prediction = model.predict(lot_area=lot_area, building_type=building_type)
+            return '<h1>Predicted House Price: {} $</h1>'.format(round(prediction[0],2))
+        return render_template('housing_prices.html', form=hpform)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- model.py
+++ model.py
@@ -1,12 +1,15 @@
 # Import packages
-import os
+import os, json
 import pickle
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-class BostonPrediction():
-    def __init__(self, data_dir):
-        self.data_dir = data_dir
+class BostonPrediction:
+    def preprocessing(self, data_df):
+        data_df.dropna(inplace=True)
+        data_df = pd.get_dummies(data_df)
+
+        return data_df
 
     def train(self):
         """
@@ -14,19 +17,21 @@ class BostonPrediction():
         """
 
         # Read train Data
-        train_dir = os.path.join(self.data_dir,'train.csv')
-        train_data_df = pd.read_csv(train_dir)
-        train_x_df = train_data_df[['LotArea','BldgType']]
+        train_data_df = pd.read_csv('Data/train.csv')
+
+        train_data_df = self.preprocessing(train_data_df[['SalePrice','LotArea','BldgType']])
+
+        train_x_df = train_data_df.drop('SalePrice', 1)
         train_y_df = train_data_df['SalePrice']
 
-        # Map BldgType to int
-        types = list(train_x_df['BldgType'].unique())
-        self.mapping = dict(zip(types, range(len(types))))
-        train_x_df.replace({'BldgType': self.mapping}, inplace=True)
+        features_dict = {'features': list(train_x_df)}
+        with open('App/model_data.json', 'w') as f:
+            json.dump(features_dict, f)
 
         # Train RandomForestRegressor
-        self.reg_model = RandomForestRegressor()
+        self.reg_model = RandomForestRegressor(oob_score=True)
         self.reg_model.fit(train_x_df, train_y_df)
+        print("test")
 
 
     def test(self):
@@ -34,26 +39,48 @@ class BostonPrediction():
         Test RandomForestRegressor on Boston House Price.
         """
         # Read test Data
-        test_dir = os.path.join(self.data_dir, 'test.csv')
-        test_data_df = pd.read_csv(test_dir)
-        test_x_df = test_data_df[['LotArea','BldgType']]
-
-        # Map BldgType to int
-        test_x_df.replace({'BldgType': self.mapping}, inplace=True)
-
+        test_data_df = pd.read_csv('Data/test.csv')
+        test_x_df = self.preprocessing(test_data_df[['LotArea','BldgType']])
         pred_y_df = self.reg_model.predict(test_x_df)
 
         print(pred_y_df)
-        print("finish")
 
-    def pickle_model(self):
-        pickle.dump(self.reg_model, open('App/model.pkl', 'wb'))
+    def predict(self, lot_area, building_type):
+        # with open('App/model_data.json', 'r') as f:
+        #     features_dict = json.load(f)
+        # self.features = features_dict['features']
+
+        data_df = pd.DataFrame(0, index=[0], columns=self.features)
+        data_df['LotArea'] = lot_area
+        bldgtype = 'BldgType_'+building_type
+        data_df[bldgtype] = 1
+        # data_prepro = self.preprocessing(pd.DataFrame(data_dict,index=[0]))
+        # predict_x_df = self.preprocessing(data_df[['LotArea', 'BldgType']])
+        # data_df.update(pd.DataFrame([data_prepro],index=[0]))
+
+
+
+
+        pred_y_df = self.reg_model.predict(data_df)
+
+        return pred_y_df
+
+    def load_model(self):
+        self.reg_model = pickle.load(open('/Users/robertwalter/PycharmProjects/Boston/App/model.pkl', 'rb'))
+        with open('/Users/robertwalter/PycharmProjects/Boston/App/model_data.json', 'r') as f:
+            features_dict = json.load(f)
+        self.features = features_dict['features']
+
+
+    def safe_model(self):
+        pickle.dump(self.reg_model, open('/Users/robertwalter/PycharmProjects/Boston/App/model.pkl', 'wb'))
 
 
 
 if __name__== "__main__":
-    data_dir = 'Data'
-    bp = BostonPrediction(data_dir)
-    bp.train()
-    bp.test()
-    bp.pickle_model()
+    bp = BostonPrediction()
+    #bp.train()
+    #bp.test()
+    #bp.safe_model()
+    bp.load_model()
+    bp.predict({"LotArea":"11250", "BldgType":"1Fam"})
